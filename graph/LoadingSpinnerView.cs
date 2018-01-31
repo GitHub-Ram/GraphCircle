8b8b3977b3d1956b563affcca52c1815c944a2b9
ramkumar <rk9629304097@gmail.com>
2018-02-01 00:49:15 +0530

    Initial check-in of module graph

---
+++ graph/LoadingSpinnerView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace graph
+{
+    public class LoadingSpinnerView : UIView
+    {
+        private CAShapeLayer _thinCirlce;
+        private CAShapeLayer _arcLayer;
+
+        public LoadingSpinnerView(nfloat radius)
+        {
+            UIColor.Red.SetColor();
+            _thinCirlce = new CAShapeLayer();
+            _thinCirlce.LineWidth = 1;
+            _thinCirlce.Path = UIBezierPath.FromOval(new CoreGraphics.CGRect(0, 0, radius * 2, radius * 2)).CGPath;
+            _thinCirlce.StrokeColor = UIColor.Red.CGColor;
+            _thinCirlce.FillColor = UIColor.Clear.CGColor;
+            Layer.AddSublayer(_thinCirlce);
+
+            UIColor.Blue.SetColor();
+            UIBezierPath arcPath = new UIBezierPath();
+            arcPath.LineWidth = 4;
+            arcPath.LineCapStyle = CGLineCap.Round;
+            arcPath.LineJoinStyle = CGLineJoin.Round;
+            arcPath.AddArc(new CoreGraphics.CGPoint(radius, radius), radius, 0, 2 * 3.14f, true);
+
+            _arcLayer = new CAShapeLayer();
+            _arcLayer.Path = arcPath.CGPath;
+            _arcLayer.StrokeColor = UIColor.Blue.CGColor;
+            _arcLayer.FillColor = UIColor.Clear.CGColor;
+            _arcLayer.LineWidth = 4;
+            _arcLayer.StrokeStart = 0;
+            _arcLayer.StrokeEnd = 1;
+
+            if (_arcLayer.SuperLayer != null)
+            {
+                _arcLayer.RemoveAllAnimations();
+                _arcLayer.RemoveFromSuperLayer();
+            }
+        }
+
+        public void StartAnimation()
+        {
+            Layer.AddSublayer(_arcLayer);
+
+            CABasicAnimation animation = new CABasicAnimation();
+            animation.KeyPath = "strokeEnd";
+            animation.Duration = 1;
+            animation.From = NSNumber.FromFloat(0);
+            animation.To = NSNumber.FromFloat(1);
+            _arcLayer.AddAnimation(animation, null);
+        }
+
+        public void StopAnimation()
+        {
+            _arcLayer.RemoveAllAnimations();
+            _arcLayer.RemoveFromSuperLayer();
+        }
+    }
+}
---
+++ graph/ViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace graph
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+        List<GraphSpecs> listSpecs = new List<GraphSpecs>();
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Dictionary<int, UIColor> colorandvalue = new Dictionary<int, UIColor>();
+            colorandvalue.Add(40,UIColor.Blue);
+            colorandvalue.Add(60, UIColor.Red);
+            colorandvalue.Add(90, UIColor.Yellow);
+            GraphSpecs specs1 = new GraphSpecs
+            {
+                percentageValuesWithColor = colorandvalue,
+                strokeWidth = 8,
+                minimumRadius = (nfloat)(View.Bounds.Width * 0.25),
+                space = 5
+            };
+            circleGraphView.setGraph(specs1);
+            Console.WriteLine("View Load");
+            //var load = new LoadingSpinnerView(60);
+            //View.AddSubview(load);
+            //load.StartAnimation();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
